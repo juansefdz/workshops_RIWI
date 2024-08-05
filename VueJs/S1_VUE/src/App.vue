f03ad5d7c4760082928c35cf5c60d6a689e19ce6
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-08-05 12:36:19 +0000

    project vueJs

---
+++ VueJs/S1_VUE/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="f-20">Hola Mundo en VUE js</h1>
+
+  <p>Contador: {{ counter }}</p>
+  <button @click ="increment">increment +</button>
+
+</template>
+
+<script>
+  console.log("hola desde consola!!!");
+
+  const counter = ref<number>(1)
+  
+  const increment = ()=>{
+    counter.value++
+  }
+</script>
+<style>
+h1 {
+  color: red;
+}
+</style>
