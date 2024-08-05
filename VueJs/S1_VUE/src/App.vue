e839a7092d599942f7f2b693e122a0b00afe1709
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-08-05 12:52:06 +0000

    update

--- VueJs/S1_VUE/src/App.vue
+++ VueJs/S1_VUE/src/App.vue
@@ -1,22 +1,8 @@
 <template>
-  <h1 class="f-20">Hola Mundo en VUE js</h1>
-
-  <p>Contador: {{ counter }}</p>
-  <button @click ="increment">increment +</button>
-
 </template>
 
-<script>
-  console.log("hola desde consola!!!");
-
-  const counter = ref<number>(1)
-  
-  const increment = ()=>{
-    counter.value++
-  }
+<script setup lang ="ts">
 </script>
+
 <style>
-h1 {
-  color: red;
-}
 </style>
---
+++ VueJs/S1_VUE/src/components/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="banner-section d-middle w-100 p-10">
+        <form >
+            <div class="search-input d-middle">
+                <input 
+                    type="text" 
+                    required ="true"
+                >
+                <span>Search Movie</span>
+            </div>
+        </form>
+        <div class="container-banner-img">
+            <img src="" alt="">
+        </div>
+    </section>
+
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
